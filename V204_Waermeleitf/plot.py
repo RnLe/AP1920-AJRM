b91c6bbf597631cec99d71fd9a5dfa5ddc8c9825
Rene Lehner <rene.lehner@tu-dortmund.de>
2019-11-20 11:58:35 +0100

    Messwerte richtig formatiert. Versuchsanleitungen hinzugefügt. Ordner für nächsten Versuch angelegt. LaTeX-Vorlage angepasst (neues package im Header). plot.py erstellt, um die drei Messreihen zu plotten. Makefile angepasst (damit er 3 plots erstellt und trackt). Hilfsmittel-Ordner erstellt und das Programm cut_data hinzugefügt, welches Messwerte reduzieren kann.
    Ende :)

--- V204_Waermeleitf/plot.py
+++ V204_Waermeleitf/plot.py
@@ -1,21 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+ID, t1, t2, t3, t4, t5, t6, t7, t8, t = np.genfromtxt("Messung_1(statisch).txt", unpack=True)
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+fig1, ax1 = plt.subplots()
+for y in (t1, t2, t3, t4, t5, t6, t7, t8):
+    ax1.plot(t, y)
+ax1.set_xlabel('$Zeit_s$')
+ax1.set_ylabel('$Temperatur_°C$')
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+ID, t1, t2, t3, t4, t5, t6, t7, t8, t = np.genfromtxt("Messung_2(dynamisch).txt", unpack=True)
+fig2, ax2 = plt.subplots()
+for y in (t1, t2, t3, t4, t5, t6, t7, t8):
+    ax2.plot(t, y)
+ax2.set_xlabel('$Zeit_s$')
+ax2.set_ylabel('$Temperatur_°C$')
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+ID, t1, t2, t3, t4, t5, t6, t7, t8, t = np.genfromtxt("Messung_3(dynamisch).txt", unpack=True)
+fig3, ax3 = plt.subplots()
+for y in (t1, t2, t3, t4, t5, t6, t7, t8):
+    ax3.plot(t, y)
+ax3.set_xlabel('$Zeit_s$')
+ax3.set_ylabel('$Temperatur_°C$')
+
+fig1.savefig('build/plot1.pdf')
+fig2.savefig('build/plot2.pdf')
+fig3.savefig('build/plot3.pdf')
