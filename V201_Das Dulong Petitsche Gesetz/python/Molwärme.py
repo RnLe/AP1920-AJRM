e6cc1ec2416f564263a113ab466bae4fcbcd195a
antoniajb <antoniajoelle.bock@udo.edu>
2019-12-01 09:38:53 +0100

    Python-Programm für Molwärme geschrieben; entsprechende Werte in Auswertung eingetragen; Fehler bei Berechnung der molekularen Wärmekapazität entdeckt und behoben;
    molekulare Wärmekapazität ist auf ein Mol bezogen; ergo musste nach Multiplikation mit der Masse (um von der spezifischen Kapazität auf die
    Kapazität zu kommen) noch durch die Stoffmenge n (quasi Anzahl der Mole) geteilt werden... jetzt haben wir für C_V auch einigermaßen
    ähnliche Werte... aber immer noch dezent entfernt von 3R... naja, das wird dann in der Diskussion diskutiert.

---
+++ V201_Das Dulong Petitsche Gesetz/python/Molwärme.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+C_p=dict(tin=[112.9,113.4,93.1],alum=[98.9,73.5,68.7],copper=103.0)
+alpha=dict(tin=27.0,alum=23.5,copper=16.8)
+kappa=dict(tin=55,alum=75,copper=136)
+V_0=dict(tin=16.3,alum=10.0,copper=7.09)
+T_m=dict(tin=[296.05,295.85,295.45],alum=[297.15,296.85,296.65],copper=296.05)
+
+C_V = dict(tin=[], alum=[], copper=0)
+
+with open("data/Molwärme.txt", 'w') as f:
+    f.write("Stoff\tMolwärme\n")
+    for material in ["tin", "alum", "copper"]:
+        if material is not "copper":
+            f.write(f"{material}")
+            for i in range(3):
+                C_V[material].append(C_p[material][i]*1000 - 9*alpha[material]**2 *(1/(10**12)) *kappa[material]* 10**9 *V_0[material]* (1/(10**6)) *T_m[material][i])
+                f.write(f"\t\t{round(C_V[material][i], 2)}\n")
+        else:
+            f.write(f"copper\t")
+            C_V[material] = (C_p[material]*1000 - 9*alpha[material]**2 *(1/(10**12)) *kappa[material]* 10**9 *V_0[material]* (1/(10**6)) *T_m[material])
+            f.write(f"{round(C_V[material], 2)}\n")
