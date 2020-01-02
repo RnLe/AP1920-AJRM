e5fb9f4068db30a8734a48af47005e05e644d036
Rene Lehner <rene.lehner@tu-dortmund.de>
2020-01-02 14:01:17 +0100

    Theoriewerte berechnet. Formeln hinzugefügt. Formatiert.

---
+++ V355_Gekoppelte_Schwingkreise/python/theory_values.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+f_plus = [81.3,61.1,57.1,48.7,44.7,42.8,41.4,40.2]
+f_minus = [33.1, 33.1, 33.1, 33.1, 33.1, 33.1, 33.1, 33.1]
+C_k_in_nano = [1.0 ,2.2 ,2.7 ,4.7 ,6.8 ,8.2 ,10.0,12.0]
+U_minus = [1.830,1.960,1.883,2.050,2.160,1.830,2.030,2.000]
+R = 48
+C_sp_in_nano = 0.037
+C_in_nano = 0.8015
+L_in_milli = 32.351
+vol_180 = 9/2
+vol_0 = 7.2/2
+
+f_plus_theo = []
+f_minus_theo = []
+cur_theo = []
+I2_plus_theo = []
+I2_minus_theo = []
+I2_theo = 0
+
+for cap in C_k_in_nano:
+    f_plus_theo.append(1 / (2*np.pi*np.sqrt(L_in_milli*(C_in_nano+C_sp_in_nano)*10**(-12))))
+
+for cap in C_k_in_nano:
+    f_minus_theo.append(1 / (2*np.pi*np.sqrt(L_in_milli*((C_in_nano*cap/(2*C_in_nano+cap))+C_sp_in_nano)*10**(-12))))
+
+for i in range(len(U_minus)):
+    I2_minus_theo.append(U_minus[i]*(1/(R*np.sqrt(4+(R**2*C_k_in_nano[i]**2/L_in_milli*C_in_nano)*(10**3)*(1+C_in_nano/C_k_in_nano[i])))))
+
+I2_theo = vol_180/(2*R) # der Wert sieht ganz gut aus. sieht so aus, als ob der manuell berechnete Leitwert nicht stimmt.
+                        # und das widerum kann (bzw. muss ja) an falschen Angaben liegen
+                        # da aber 1/2R als Leitwert unserem gemessenen Wert nahekommt, wird R = 48 Ohm wohl richtig sein
+
+print("f_plus_theo = ")
+for val in f_plus_theo:
+    print(val)
+#print(f_plus_theo)
+print("\nf_minus_theo = ")
+for val in f_minus_theo:
+    print(val)
+#print(f_minus_theo)
+print("\nI2_minus_theo = ")
+for val in I2_minus_theo:
+    print(val)
+#print(I2_minus_theo)
+print("\nI2_theo = ")
+print(I2_theo)
