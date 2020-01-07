d3f2e43bfadef6c0930cfed933b3f55707b94fc0
Rene Lehner <rene.lehner@tu-dortmund.de>
2020-01-07 15:49:03 +0100

    Messwerte und verschiedenes.

---
+++ V107_Hoeppler_Viskosimeter/python/plot.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import matplotlib as mpl
+mpl.use('pgf')
+import matplotlib.pyplot as plt
+mpl.rcParams.update({
+'font.family': 'serif',
+'text.usetex': True,
+'pgf.rcfonts': False,
+'pgf.texsystem': 'lualatex',
+'pgf.preamble': r'\usepackage{unicode-math}\usepackage{siunitx}',
+})
+
+x = np.linspace(0, 10, 1000)
+y = x ** np.sin(x)
+
+plt.subplot(1, 2, 1)
+plt.plot(x, y, label='Kurve')
+plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
+plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
+plt.legend(loc='best')
+
+plt.subplot(1, 2, 2)
+plt.plot(x, y, label='Kurve')
+plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
+plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
+plt.legend(loc='best')
+
+# in matplotlibrc leider (noch) nicht möglich
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/plot.pdf')
