c9ac94a288443104981ffdf52c17951a34eba3e2
RnLe <rene.lehner@udo.edu>
2020-06-08 21:09:06 +0200

    V406 Auswertung bearbeitet

---
+++ V406/python/DoppelspaltFit.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+from scipy.optimize import curve_fit
+
+mpl.use('pgf')
+mpl.rcParams.update({
+'font.family': 'serif',
+'text.usetex': True,
+'pgf.rcfonts': False,
+'pgf.texsystem': 'lualatex',
+'pgf.preamble': r'\usepackage{unicode-math}\usepackage{siunitx}',
+})
+
+# y-Achse ist µA und x-Achse mm
+I2 = np.genfromtxt('../data/Messdaten_V406_1.txt', comments='#', unpack=True)
+I2[1] = I2[1]
+lam = 633*10**-6
+_d = 626.1
+Id = 0.7         # Dunkelstrom
+
+
+def forReal(x, x0, a, b, c):
+    x_ = abs(x - x0)
+    d_ = np.sqrt(_d**2 + x_**2)
+    sin_phi = x_ / d_
+    eta = (np.pi * b * sin_phi) / lam
+    return ((a*b*np.sinc(eta))**2 + c)*10**6
+
+# die vorgegebenen Startwere p0 sind wichtig, da der Fit ansonsten total daneben ist. x0 kannten wir ja schon in etwa. Den Dunkelstrom und die Spaltbreite auch
+params, cov = curve_fit(forReal, I2[0], I2[1], p0=[12, 1, 0.075, Id])
+
+x1 = np.linspace(0, I2[0][-1], 200)
+plt.plot(I2[0], I2[1], 'b.')
+plt.plot(x1, forReal(x1, *params), 'g')
+plt.xlabel(r'x \:/\: mm')
+plt.ylabel(r'I \:/\: µA')
+
+uncertainties = np.sqrt(np.diag(cov))
+
+for name, value, uncertainty in zip('xabI', params, uncertainties): 
+    print(f'{name} = {value:8.8f} ± {uncertainty:.8f}')
+
+plt.savefig('EinzelspaltFit.pdf')
