ac111ace406048cfbdc4ef3e0554273532932555
ToniAJ <antoniaj.bock@t-online.de>
2020-07-17 13:05:06 +0200

    bei V601 fehlen jetzt nur noch die Diskussion und die Quellenverweise

--- V601_Franck-Hertz/python/plot.py
+++ V601_Franck-Hertz/python/plot.py
@@ -18,23 +18,44 @@ mpl.rcParams.update({
 
 U_A,I_A=np.genfromtxt("../data/energie.csv",delimiter=",",unpack=True)
 
-plt.plot(U_A,I_A,"x",color='indianred',label='Messwerte')
-plt.ylabel(r'$I_\text{A}\,/\,\si{\nano\ampere}$')
-plt.xlabel(r'$U_\text{A}\,/\,\si{\volt}$')
-plt.grid(':')
-plt.legend()
-plt.savefig("../plots/Energie.pdf")
+#plt.plot(U_A,I_A,"x",color='indianred',label='Messwerte')
+#plt.ylabel(r'$I_\text{A}\,/\,\si{\nano\ampere}$')
+#plt.xlabel(r'$U_\text{A}\,/\,\si{\volt}$')
+#plt.grid(':')
+#plt.legend()
+#plt.savefig("../plots/Energie.pdf")
+#
+#plt.clf()
+
+#plt.plot(U_A,I_A,":",color='indianred',label='Messwerte')
+#plt.ylabel(r'$I_\text{A}\,/\,\si{\nano\ampere}$')
+#plt.xlabel(r'$U_\text{A}\,/\,\si{\volt}$')
+#plt.grid(':')
+#plt.legend()
+#plt.savefig("../plots/Energie2.pdf")
 
 plt.clf()
 
 a=I_A.size-1
-Diff_I=np.zeros(a)
+Diff_I=np.zeros(a+1)
 for i in range(a):
     Diff_I[i]=I_A[i]-I_A[i+1]
 
-plt.plot(U_A[:a],Diff_I,"x",color="lightcoral",label="Messwerte")
-plt.ylabel(r'$(I_\text{A}(U)-I_\text{A}(U+\Delta U))\,/\,\si{\nano\ampere}$')
-plt.xlabel(r'$U_\text{A}\,/\,\si{\volt}$')
-plt.grid(':')
-plt.legend()
-plt.savefig("../plots/EnergieDiff.pdf")
+#plt.plot(U_A[:a],Diff_I,"x",color="lightcoral",label="Messwerte")
+#plt.ylabel(r'$(I_\text{A}(U)-I_\text{A}(U+\Delta U))\,/\,\si{\nano\ampere}$')
+#plt.xlabel(r'$U_\text{A}\,/\,\si{\volt}$')
+#plt.grid(':')
+#plt.legend()
+#plt.savefig("../plots/EnergieDiff.pdf")
+#
+#plt.clf()
+
+#plt.plot(U_A,Diff_I,':',color="lightcoral",label="Messwerte")
+#plt.ylabel(r'$(I_\text{A}(U)-I_\text{A}(U+\Delta U))\,/\,\si{\nano\ampere}$')
+#plt.xlabel(r'$U_\text{A}\,/\,\si{\volt}$')
+#plt.grid(':')
+#plt.legend()
+#plt.savefig("../plots/EnergieDiff2.pdf")
+
+
+np.savetxt('../data/energy.txt', np.column_stack([U_A, I_A, Diff_I]), header="U_A \t I_A \t Delta I_A")
